558d96035df360c24f681df0c277d5cb69a24f1b
Joe van Bohemen <joseph.vanbohemen@milkbooks.com>
2022-09-21 11:58:40 +1200

    oops forgot rebuild

--- lib/playlists.d.ts
+++ lib/playlists.d.ts
@@ -10,7 +10,7 @@ export interface ISpotifyPlaylist {
     };
     name: string;
     owner: ISpotifyProfile;
-    tracks: ISpotifyPaginatedResponse<ISpotifyTrack>[];
+    tracks: ISpotifyPaginatedResponse<ISpotifyPlaylistTrack>[];
 }
 export interface ISpotifyPlaylistTrack {
     added_at: string;
